145fb76291b276e7e4e7eca4aecadb883976ec35
Happy1speed <aoceansanny@gmail.com>
2025-11-20 10:48:19 -0800

    Thats great, some fixes to this file

--- src/AccountCreation.java
+++ src/AccountCreation.java
@@ -1,13 +1,13 @@
-public class AccountCreation extends Saveable() {
+public class AccountCreation {
     
     public static void createAccount(String userName, String email, String password, boolean isTeacher) {
         if(isTeacher == true) {
-            TeacherAccount teacher = new TeacherAccount(String userName, String email, String password);
+            TeacherAccount teacher = new TeacherAccount(userName, email, password);
             teacher.save();
             GlobalData.teacherList.add(teacher);
         }
         else {
-            StudentAccount student = new StudentAccount(String userName, String email, String password);
+            StudentAccount student = new StudentAccount(userName, email, password, 0);
             student.save();
             GlobalData.studentList.add(student);
         }
