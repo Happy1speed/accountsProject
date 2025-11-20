ef1324f1b1444af4509258215a2a5ebc05e497a1
Happy1speed <144567994+Happy1speed@users.noreply.github.com>
2025-11-19 22:00:53 -0800

    Added account list

--- src/GlobalData.java
+++ src/GlobalData.java
@@ -5,6 +5,7 @@ public class GlobalData {
 
     public static ArrayList<TeacherAccount> teacherList = new ArrayList<TeacherAccount>();
     public static ArrayList<StudentAccount> studentList = new ArrayList<StudentAccount>();
+    public static ArrayList<BaseAccount> accountList = new ArrayList<BaseAccount>();
     public static ArrayList<Assignment> assignmentList = new ArrayList<Assignment>();
     public static ArrayList<Saveable> saveableList = new ArrayList<Saveable>();
 }
